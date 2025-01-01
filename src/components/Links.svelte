f472ed4dededd5de1847a0c620f1e3667a52253d
Jack Ketcham <jack@after.com>
2024-12-31 17:07:24 -0700

    feat: Enhance Links component to open external links in a new tab
    - Updated the Links component to conditionally set target and rel attributes for external links, improving user experience and security.

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -8,6 +8,7 @@
 		<a
 			href={link_url}
 			class="block w-full bg-white hover:bg-neutral-50 transition rounded-lg p-6 text-center font-display uppercase font-bold text-xl border border-neutral-200"
+			{...!link_url.startsWith('/') ? { target: '_blank', rel: 'noopener noreferrer' } : {}}
 		>
 			{link_label}
 		</a>
