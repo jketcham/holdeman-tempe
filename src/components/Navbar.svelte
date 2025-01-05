1ee4cc26f061eb7d39e91f8f238715345625b3fc
Jack Ketcham <jack@after.com>
2025-01-05 11:45:17 -0700

    feat: Adjust spacing of navbar header elements

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,15 +10,23 @@
     <Container>
       <Weather {initialWeather} />
       <a href="/">
-        <div class="flex justify-between py-5 font-display uppercase md:py-8">
-          <div class="flex flex-shrink flex-col">
-            <span class="block text-2xl font-bold md:mb-2 md:text-5xl">Holdeman</span>
-            <span class="block text-lg font-bold md:text-2xl">Neighborhood</span>
-            <span class="block text-lg font-bold md:text-2xl">Association</span>
-          </div>
-          <div class="flex flex-shrink-0 flex-col text-right">
-            <span class="mt-auto block text-lg font-bold md:mb-2 md:text-2xl">Est. 1998</span>
-            <span class="block text-xs md:text-xl">holdemantempe.org</span>
+        <div class="flex flex-col justify-between py-4 font-display uppercase sm:py-5 md:py-8">
+          <span class="mb-1 block text-3xl font-bold leading-7 sm:leading-9 md:mb-2 md:text-5xl"
+            >Holdeman</span
+          >
+          <div class="flex justify-between">
+            <div class="flex flex-shrink flex-col">
+              <span class="block text-lg font-bold leading-5 sm:leading-7 md:text-2xl"
+                >Neighborhood</span
+              >
+              <span class="block text-lg font-bold leading-5 sm:leading-7 md:text-2xl"
+                >Association</span
+              >
+            </div>
+            <div class="flex flex-shrink-0 flex-col justify-between text-right">
+              <span class="block text-lg font-bold leading-5 sm:mt-auto md:text-2xl">Est. 1998</span>
+              <span class="block text-xs md:text-xl">holdemantempe.org</span>
+            </div>
           </div>
         </div>
       </a>
